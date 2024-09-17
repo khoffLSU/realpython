aad9f347011cccc162cff97193e1263b39456fb0
Keith Hoffman <keithnhoffman@gmail.com>
2024-09-16 21:16:14 -0500

    Add title length calculation

--- walrus/walrus_pod.py
+++ walrus/walrus_pod.py
@@ -1,5 +1,8 @@
 from reader import feed
-import parse
+import parse, statistics
+
+# RSS Feed for podcast titles
+url = "https://realpython.com/podcasts/rpp/feed"
 
 # pattern to match in the feed
 pattern = parse.compile(
@@ -22,3 +25,12 @@ podcasts = [
 
 for i in podcasts[:10]:
     print(i) 
+
+
+title_len = statistics.mean(
+    title_length 
+    for title in podcasts 
+    if (title_length := len(title)) > 50
+)
+
+print(title_len)
