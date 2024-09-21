497dabe3334ef609829f1df60ce3fbd35e7c4d96
Keith Hoffman <keithnhoffman@gmail.com>
2024-09-20 23:13:54 -0500

    Add Unit tests

--- wordle/wydle.py
+++ wordle/wydle.py
@@ -8,7 +8,8 @@ from string import ascii_letters
 
 def main():
     # Pre Process
-    word = get_random_word()
+    word_path = pathlib.Path(__file__).parent / "wordlist.txt"
+    word = get_random_word(word_path.read_text(encoding="utf-8").split("\n"))
 
     # Process (Main Loop)
     for guess_num in range(1,7):
@@ -24,11 +25,13 @@ def main():
         game_over(word)
 
 
-def get_random_word():
-    '''Pull random word from text corpus for game'''
-    wordlist = pathlib.Path(__file__).parent / "wordlist.txt"
-
-    words = [word.upper() for word in wordlist.read_text(encoding="utf-8").split("\n")
+def get_random_word(word_list):
+    '''Pull random word from text corpus for game
+      ## Example:
+      >>> get_random_word(["snake", "worm", "it'll"])
+      'SNAKE' 
+    '''
+    words = [word.upper() for word in word_list
          if len(word) == 5 and all(letter in ascii_letters for letter in word)
          ]
     
@@ -36,13 +39,21 @@ def get_random_word():
 
 def show_guess(guess, word):
     '''Show break down of word guessed versus secret word in terms of letters
-       Correct, missplaced and incorrect letters'''
+       Correct, missplaced and incorrect letters
+       
+       ## Example:
+
+       >>> show_guess("CRANE","SNAKE")
+       Correct letters: A, E
+       Misplaced letters: N
+       Wrong letters: C, R
+       '''
     correct_letters = {letter for letter,correct in zip(guess,word) if letter == correct}
     missplaced_letters = set(guess)&set(word) - correct_letters
     incorrect_letters = set(guess)-set(word)
-    print("Correct Letters: ",", ".join(correct_letters))
-    print("Missplaced Letters: ",", ".join(missplaced_letters))
-    print("Incorrect Letters: ",", ".join(incorrect_letters))
+    print("Correct letters:",", ".join(sorted(correct_letters)))
+    print("Misplaced letters:",", ".join(sorted(missplaced_letters)))
+    print("Wrong letters:",", ".join(sorted(incorrect_letters)))
 
 def game_over(word):
     '''Finish game message upom losing'''
