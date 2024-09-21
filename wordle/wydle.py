48bd033e08e8fd92cbd722ad35093670617711d5
Keith Hoffman <keithnhoffman@gmail.com>
2024-09-21 15:58:19 -0500

    Add Rich formating

--- wordle/wydle.py
+++ wordle/wydle.py
@@ -4,20 +4,27 @@
 import pathlib
 import random
 from string import ascii_letters
-    
+from rich.console import Console
+from rich.theme import Theme
+
+console = Console(width = 40, theme = Theme({"warning": "red on yellow"}))
 
 def main():
     # Pre Process
     word_path = pathlib.Path(__file__).parent / "wordlist.txt"
     word = get_random_word(word_path.read_text(encoding="utf-8").split("\n"))
+    guesses = ["_" * 5] * 6
 
     # Process (Main Loop)
-    for guess_num in range(1,7):
-        guess = input(f"\n{guess_num}. Guess a word: ").upper()
+    for idx in range(6):
+        refresh_page(headline = f"Guess {idx + 1}")
+        show_guesses(guesses, word)
 
-        show_guess(guess, word)
+        guesses[idx] = input("\nGuess word: ").upper()
 
-        if guess == word:
+        if guesses[idx] == word:
+            show_guesses(guesses, word)
+            print("CORRECT! YOU WIN!")
             break
     
     # Post Process
@@ -27,15 +34,34 @@ def main():
 
 def get_random_word(word_list):
     '''Pull random word from text corpus for game
-      ## Example:
-      >>> get_random_word(["snake", "worm", "it'll"])
-      'SNAKE' 
+
+    ## Example:
+    
+    >>> get_random_word(["snake", "worm", "it'll"])
+    'SNAKE'
     '''
-    words = [word.upper() for word in word_list
-         if len(word) == 5 and all(letter in ascii_letters for letter in word)
-         ]
+    words = [
+        word.upper() for word in word_list
+        if len(word) == 5 and all(letter in ascii_letters for letter in word)
+        ]
     
     return random.choice(words)
+
+def show_guesses(guesses, word):
+    for guess in guesses:
+        styled_guess = [] # initalize a list
+        for letter, correct in zip(guess, word):
+            if letter == correct:
+                style = "bold white on green"
+            elif letter in word:
+                style = "bold white on yellow"
+            elif letter in ascii_letters:
+                style = "red on #666666"
+            else:
+                style = "dim"
+            styled_guess.append(f"[{style}]{letter}[/]")
+        
+        console.print("".join(styled_guess), justify="center")
 
 def show_guess(guess, word):
     '''Show break down of word guessed versus secret word in terms of letters
@@ -58,6 +84,11 @@ def show_guess(guess, word):
 def game_over(word):
     '''Finish game message upom losing'''
     print(f'The word was {word}')
+
+def refresh_page(headline):
+    '''Page refresh for Wordle game'''
+    console.clear()
+    console.rule(f"[bold blue]:leafy_green: {headline} :leafy_green:[/]\n")
 
 if __name__ == "__main__":
     main()
