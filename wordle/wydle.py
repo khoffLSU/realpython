30d697aa752fd3ec5c1a67af254331ef5706e741
Keith Hoffman <keithnhoffman@gmail.com>
2024-09-20 21:40:22 -0500

    add letter checks

--- wordle/wydle.py
+++ wordle/wydle.py
@@ -1,9 +1,24 @@
 # wyrdle.py
 
 '''Python Wordle game based on tutorial on realpython.com by Geir Arne Hjelle.'''
+import pathlib
+import random
 
-# Using Walrus operator to combine condition and assignment
-if (guess := input("Guess a word: ")).upper() == "SNAKE":
-    print("Correct")
-else:
+
+WORD = "SNAKE"
+
+for guess_num in range(1,7):
+    # Using Walrus operator to combine condition and assignment
+    guess = input(f"\n{guess_num} Guess a word: ").upper()
+    if guess == WORD:
+        print("Correct")
+        break
+    
     print("Wrong")
+
+    correct_letters = {letter for letter,correct in zip(guess,WORD) if letter == correct}
+    missplaced_letters = set(guess)&set(WORD) - correct_letters
+    incorrect_letters = set(guess)-set(WORD)
+    print("Correct Letters: ",", ".join(correct_letters))
+    print("Missplaced Letters: ",", ".join(missplaced_letters))
+    print("Incorrect Letters: ",", ".join(incorrect_letters))
