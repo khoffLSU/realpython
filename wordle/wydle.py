499b60538941fbf480169c50e06bfe962e418fff
Keith Hoffman <keithnhoffman@gmail.com>
2024-09-21 23:12:44 -0500

    Add constants and user assist messages

--- wordle/wydle.py
+++ wordle/wydle.py
@@ -9,19 +9,22 @@ from rich.theme import Theme
 
 console = Console(width = 40, theme = Theme({"warning": "red on yellow"}))
 
+NUM_LETTERS = 5
+NUM_GUESSES = 6
+WORDS_PATH = pathlib.Path(__file__).parent / "wordlist.txt"
+
 def main():
     # Pre Process
     word_path = pathlib.Path(__file__).parent / "wordlist.txt"
     word = get_random_word(word_path.read_text(encoding="utf-8").split("\n"))
-    guesses = ["_" * 5] * 6
+    guesses = ["_" * NUM_LETTERS] * NUM_GUESSES
 
     # Process (Main Loop)
-    for idx in range(6):
+    for idx in range(NUM_GUESSES):
         refresh_page(headline = f"Guess {idx + 1}")
         show_guesses(guesses, word)
 
-        guesses[idx] = input("\nGuess word: ").upper()
-
+        guesses[idx] = guess_word(previous_guesses=guesses[:idx])
         if guesses[idx] == word:
             break
     
@@ -37,12 +40,14 @@ def get_random_word(word_list):
     >>> get_random_word(["snake", "worm", "it'll"])
     'SNAKE'
     '''
-    words = [
+    if words := [
         word.upper() for word in word_list
         if len(word) == 5 and all(letter in ascii_letters for letter in word)
-        ]
-    
-    return random.choice(words)
+        ]:
+            return random.choice(words)
+    else:
+        console.print("No words of length 5 in word list", style = "warning")
+        raise SystemExit()
 
 def show_guesses(guesses, word):
     for guess in guesses:
@@ -51,7 +56,7 @@ def show_guesses(guesses, word):
             if letter == correct:
                 style = "bold white on green"
             elif letter in word:
-                style = "bold white on yellow"
+                style = "bold white on blue"
             elif letter in ascii_letters:
                 style = "bold white on red"
             else:
@@ -92,6 +97,27 @@ def refresh_page(headline):
     '''Page refresh for Wordle game'''
     console.clear()
     console.rule(f"[bold blue]:leafy_green: {headline} :leafy_green:[/]\n")
+
+def guess_word(previous_guesses):
+    '''Function to handle user guess and give feedback'''
+    guess = console.input("\nGuess word:").upper()
+
+    if guess in previous_guesses:
+        console.print(f"You've already guessed {guess}.", style = "warning")
+        return guess_word(previous_guesses)
+    
+    if len(guess) < 5:
+        console.print("Your guess must be 5 letters.", style = "warning")
+        return guess_word(previous_guesses)
+    
+    if any((invalid := letter) not in ascii_letters for letter in guess):
+        console.print(
+            f"Invalid letter: {invalid}. Please use English letters.",
+            style = "warning"
+        )
+        return guess_word(previous_guesses)
+
+    return guess
 
 if __name__ == "__main__":
     main()
